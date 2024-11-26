bb5d088738822fa916c25fe3dc09604446de2330
jhandry naula <jjnaula3@utpl,edu,ec>
2024-11-25 20:41:11 -0500

    problema 3

---
+++ problematicas/problema003/src/main/java/com/mycompany/problema003/Problema003.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.problema003;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Problema003 {
+
+    public static void main(String[] args) {
+        // Inicializar variables
+        int numero = 2;
+        int incremento = 3; 
+
+      for (int i = 0; i < 6; i++) { 
+    System.out.println(numero);
+    numero = numero + incremento; 
+    incremento = incremento + 2; 
+      }
+    } 
+}
+
+
+    
+    
+
+    
